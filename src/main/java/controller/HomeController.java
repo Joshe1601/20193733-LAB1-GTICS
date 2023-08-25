109cd8927b3ad70f0efddf9bb19a4b9f54b5af99
Jose Morillos <jmorillosp@pucp.edu.pe>
2023-08-24 20:45:57 -0500

    Hasta acá el ejercicio 1 y 2 y la primera linea del ejercicio 3

---
+++ src/main/java/controller/HomeController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class HomeController {
+    @GetMapping(value = "/principal")
+    public String principal() {
+        return "principal";
+    }
+}
