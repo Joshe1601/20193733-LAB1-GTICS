76a594b9df17ff06566cabf92f4a4d6ea6b76458
Jose Morillos <jmorillosp@pucp.edu.pe>
2023-08-24 20:37:30 -0500

    Repositorio creado

---
+++ src/main/java/com/example/lab120193733gtics/Lab120193733GticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab120193733gtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab120193733GticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab120193733GticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lab120193733gtics/Lab120193733GticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab120193733gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab120193733GticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
